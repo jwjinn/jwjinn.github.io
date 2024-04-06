5c617fda24c4cf5be8ee8ad0fa13caaeb5f2ad44
jwjinn <jwjinn@gmail.com>
2024-04-06 18:44:02 +0900

    sidebar 수정 2

--- _javascript/modules/components/sidebar.js
+++ _javascript/modules/components/sidebar.js
@@ -1,58 +1,58 @@
-// /**
-//  * Expand or close the sidebar in mobile screens.
-//  */
+/**
+ * Expand or close the sidebar in mobile screens.
+ */
 
-// const $body = $('body');
-// const ATTR_DISPLAY = 'sidebar-display';
+const $body = $('body');
+const ATTR_DISPLAY = 'sidebar-display';
 
-// class SidebarUtil {
-//   static isExpanded = false;
+class SidebarUtil {
+  static isExpanded = false;
 
-//   static toggle() {
-//     if (SidebarUtil.isExpanded === false) {
-//       $body.attr(ATTR_DISPLAY, '');
-//     } else {
-//       $body.removeAttr(ATTR_DISPLAY);
-//     }
+  static toggle() {
+    if (SidebarUtil.isExpanded === false) {
+      $body.attr(ATTR_DISPLAY, '');
+    } else {
+      $body.removeAttr(ATTR_DISPLAY);
+    }
 
-//     SidebarUtil.isExpanded = !SidebarUtil.isExpanded;
-//   }
-// }
+    SidebarUtil.isExpanded = !SidebarUtil.isExpanded;
+  }
+}
 
-// export function sidebarExpand() {
-//   $('#sidebar-trigger').on('click', SidebarUtil.toggle);
-//   $('#mask').on('click', SidebarUtil.toggle);
-// }
+export function sidebarExpand() {
+  $('#sidebar-trigger').on('click', SidebarUtil.toggle);
+  $('#mask').on('click', SidebarUtil.toggle);
+}
 
 /**
  * Expand or close the sidebar in mobile screens.
  */
 
-$(function () {
-  const sidebarUtil = (function () {
-    const ATTR_DISPLAY = 'sidebar-display';
-    let isExpanded = false;
-    const body = $('body');
+// $(function () {
+//   const sidebarUtil = (function () {
+//     const ATTR_DISPLAY = 'sidebar-display';
+//     let isExpanded = false;
+//     const body = $('body');
 
-    return {
-      toggle() {
-        if (isExpanded === false) {
-          body.attr(ATTR_DISPLAY, '');
-        } else {
-          body.removeAttr(ATTR_DISPLAY);
-        }
+//     return {
+//       toggle() {
+//         if (isExpanded === false) {
+//           body.attr(ATTR_DISPLAY, '');
+//         } else {
+//           body.removeAttr(ATTR_DISPLAY);
+//         }
 
-        isExpanded = !isExpanded;
-      }
-    };
-  })();
+//         isExpanded = !isExpanded;
+//       }
+//     };
+//   })();
 
-  // $('#sidebar-trigger').click(sidebarUtil.toggle);
-  // $("#sidebar-trigger").click(function(){console.log("hello")})
-  $('#sidebar-trigger').click(function () {
-    console.log('hello');
-    sidebarUtil.toggle();
-  });
+//   // $('#sidebar-trigger').click(sidebarUtil.toggle);
+//   // $("#sidebar-trigger").click(function(){console.log("hello")})
+//   $('#sidebar-trigger').click(function () {
+//     console.log('hello');
+//     sidebarUtil.toggle();
+//   });
 
-  $('#mask').click(sidebarUtil.toggle);
-});
+//   $('#mask').click(sidebarUtil.toggle);
+// });
