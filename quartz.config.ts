c958b9b3d73c726cdcd6640ca8c4742546c9eda1
jwjinn <jwjinn@gmail.com>
2025-12-14 12:39:05 +0900

    Quartz sync: Dec 14, 2025, 12:39 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "우진's 아카이빙",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
